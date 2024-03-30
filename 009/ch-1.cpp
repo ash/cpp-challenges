07be0562a16a7bf7b8aab14f47092820cf7c6e0f
Andrew Shitov <mail@andreyshitov.com>
2024-03-30 11:27:55 +0100

    Week 9 Task 1

---
+++ 009/ch-1.cpp
@@ -0,0 +1,41 @@
+// Solution of Task 1 of The Weekly Challenge 9
+// https://theweeklychallenge.org/blog/perl-weekly-challenge-009/
+
+// Test run:
+// $ java ch-1.java
+// n = 113
+// n^2 = 12769
+
+#include<math.h>
+#include<string>
+#include<iostream>
+#include<map>
+
+using namespace std;
+
+int countDigits(int n) {
+    string s = to_string(n);
+    map<char, int> digits;
+
+    for (int c = 0; c != s.size(); c++) {
+        digits[s[c]] = 1;
+    }
+
+    return digits.size();
+}
+
+int main() {
+    int n = (int) sqrt(10000); // To start with a number whose square has 5 digits.
+    bool isFound = false;
+    while(!isFound) {
+        int n2 = n * n;
+        if (countDigits(n2) >= 5) {
+            isFound = true;
+            cout << "n = " << n << endl;
+            cout << "n^2 = " << n2 << endl;
+        }
+        else {
+            n++;
+        }
+    }        
+}
